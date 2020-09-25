369ff6ecfee0a6e3ad043e86a34ed5c3b56e09f7
student <artem_rasskazov0406011488@mail.ru>
2020-09-25 17:43:26 +0300

    fix+

--- practika3(raskazov)/Program.cs
+++ practika3(raskazov)/Program.cs
@@ -9,22 +9,13 @@ namespace practika3
   {
     static void Main(string[] args)
     {
-      string[] input = { "(555)555-1212",
-                              "(555) 555-1212",
-                              "555-555-1212",
-                              "5555551212",
-                              "01111",
-                              "01111-1111",
-                              "47",
-                              "111-11-1111",
-                              "8005555535"};
+      string[] input = { "(555)555-1212", "(555) 555-1212", "555-555-1212", "5555551212", "01111", "01111-1111", "47", "111-11-1111" };
       foreach (string s in input)
       {
-        if (IsPhone(s)) Console.WriteLine(s + " is a phone number");
+        if (IsPhone(s)) Console.WriteLine(ReformatPhone(s) + " is a phone number");
         else if (IsZip(s)) Console.WriteLine(s + " is a zip code");
         else Console.WriteLine(s + " is unknown");
       }
-      Console.ReadLine();
     }
 
     static bool IsPhone(string s)
@@ -35,6 +26,12 @@ namespace practika3
     static bool IsZip(string s)
     {
       return Regex.IsMatch(s, @"^\d{5}(\-\d{4})?$");
+    }
+
+    static string ReformatPhone(string s)
+    {
+      Match m = Regex.Match(s, @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$");
+      return String.Format("({0}) {1}-{2}", m.Groups[1], m.Groups[2], m.Groups[3]);
     }
   }
 }
